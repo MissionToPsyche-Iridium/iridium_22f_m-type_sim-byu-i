cc8bc5528e9ceb942078b5f837d848458290b36c
Jerry Lane <55466939+JRLane-CS@users.noreply.github.com>
2024-11-12 06:31:27 -0600

    tightened the Welcome component

--- MASS/mass.client/src/components/Welcome.jsx
+++ MASS/mass.client/src/components/Welcome.jsx
@@ -11,17 +11,7 @@ function Welcome() {
                 height: "100%"
             }} >
                 <h1 >
-                    Welcome to the Psyche
-                </h1>
-            </div>
-            
-            <div style={{
-                display: "flex",
-                alignItems: "inherit",
-                justifyContent: "center",
-                height: "100%"
-            }} >
-                <h1>
+                    Welcome to the Psyche <br />
                     Mission Home Page
                 </h1>
             </div>
