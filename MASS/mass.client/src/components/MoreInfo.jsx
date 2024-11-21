563eb83b1a36a484001a0d7a94c6988f905f5aed
Jerry Lane <55466939+JRLane-CS@users.noreply.github.com>
2024-11-21 10:28:48 -0600

    Linked Home page to More Info page

---
+++ MASS/mass.client/src/components/MoreInfo.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+function MoreInfo() {
+
+
+    return (
+        <div>
+            <h1>
+                This is where the More Information goes!
+            </h1>
+
+            <button size="large" type="button" onClick={""}>
+                Return to Home page
+            </button>
+        </div>
+    );
+}
+
+export default MoreInfo
