9dbc13a5e5748aa6f30a20caf6ac3c5b95e8f1f8
Jerry Lane <55466939+JRLane-CS@users.noreply.github.com>
2024-11-30 16:04:08 -0600

    Updated MoreInfo component to remove list items

--- MASS/mass.client/src/components/MoreInfo.jsx
+++ MASS/mass.client/src/components/MoreInfo.jsx
@@ -7,190 +7,122 @@ function MoreInfo() {
     const handleButtonClick = () => {
         navigate('/');
     };
-    
+
     return (
 
-        <div>
-            <>
-                {/*<header>
-                <p>header</p>
-                <div class="logo">
-                    <a target="_blank" href="https://www.nasa.gov/">
-                        <img src="https://psyche.asu.edu/wp-content/themes/psyche/static/img/nasa.svg" alt="NASA Logo" />
-                    </a>
-                    <span class="divider"></span>
-                    <a target="_blank" href="https://psyche.asu.edu/">
-                        <img src="https://psyche.asu.edu/wp-content/themes/psyche/static/img/psyche.svg" alt="Psyche Logo" />
-                    </a>
-                </div>
-            </header>*/}
+        <>
 
-                <button title="navigation" id="hamburgerButton"></button>
-                <nav>
-                    <ul>
+            <h1>More Information Page</h1>
+            <p>
+                <b>
+                    Check out these links to learn more about NASA's Psyche Mission! Psyche is an asteroid believed to be <br />
+                    made of metal, possibly the remanant of a planetary core. We invite you to examine the mission with an <br />
+                    explorer's sense of adventure. Who knows what we'll find when we reach the Psyche asteroid.</b>
+            </p><br />
 
-                        <h3>Official Resources</h3>
-                        <li><a href="https://psyche.asu.edu/" target="_blank">Official Psyche Mission Website</a></li>
-                        <li><a href="https://www.nasa.gov/psyche" target="_blank">NASA's Psyche Page</a></li>
-                        <li><a href="https://www.nasa.gov/multimedia/nasatv/" target="_blank">NASA TV</a></li>
+            <h3>Official Resources</h3>
+            <a href="https://psyche.asu.edu/" target="_blank">Official Psyche Mission Website</a><br />
+            <a href="https://www.nasa.gov/psyche" target="_blank">NASA's Psyche Page</a><br />
+            <a href="https://www.nasa.gov/multimedia/nasatv/" target="_blank">NASA TV</a>
 
 
-                        <h3>Articles and News</h3>
-                        <li><a href="https://blogs.nasa.gov/" target="_blank">NASA Blog Updates</a></li>
-                        <li><a href="https://www.space.com/" target="_blank">Space.com - Psyche Coverage</a></li>
-                        <li><a href="https://www.scientificamerican.com/" target="_blank">Scientific American</a></li>
+            <h3>Articles and News</h3>
+            <a href="https://blogs.nasa.gov/" target="_blank">NASA Blog Updates</a><br />
+            <a href="https://www.space.com/" target="_blank">Space.com - Psyche Coverage</a><br />
+            <a href="https://www.scientificamerican.com/" target="_blank">Scientific American</a>
 
 
-                        <h3>Educational Resources</h3>
-                        <li><a href="https://psyche.asu.edu/get-involved/psyche-inspired/" target="_blank">Psyche Inspired Program</a></li>
-                        <li><a href="https://www.nasa.gov/stem" target="_blank">NASA STEM Resources</a></li>
+            <h3>Educational Resources</h3>
+            <a href="https://psyche.asu.edu/get-involved/psyche-inspired/" target="_blank">Psyche Inspired Program</a><br />
+            <a href="https://www.nasa.gov/stem" target="_blank">NASA STEM Resources</a>
 
 
-                        <h3>Videos and Media</h3>
-                        <li><a href="https://www.youtube.com/user/NASAtelevision" target="_blank">NASA YouTube Channel</a></li>
+            <h3>Videos and Media</h3>
+            <a href="https://www.youtube.com/user/NASAtelevision" target="_blank">NASA YouTube Channel</a>
 
 
-                        <h3>Interactive Tools</h3>
-                        <li><a href="https://eyes.nasa.gov/apps/asteroids/" target="_blank">NASA Eyes on Asteroids</a></li>
+            <h3>Interactive Tools</h3>
+            <a href="https://eyes.nasa.gov/apps/asteroids/" target="_blank">NASA Eyes on Asteroids</a>
 
 
-                        <h3>Podcasts and Audio Resources</h3>
-                        <li><a href="https://www.nasa.gov/nasapodcasts" target="_blank">NASA?s On a Mission Podcast</a></li>
-                        <li><a href="https://www.planetary.org/planetary-radio" target="_blank">Planetary Radio by The Planetary Society</a></li>
+            <h3>Podcasts and Audio Resources</h3>
+            <a href="https://www.nasa.gov/nasapodcasts" target="_blank">NASA's On a Mission Podcast</a><br />
+            <a href="https://www.planetary.org/planetary-radio" target="_blank">Planetary Radio by The Planetary Society</a>
 
-                        <h3>Books And Papers</h3>
-                        <li><a href="https://arxiv.org/" target="_blank">arXiv Scientific Publications</a></li>
-                        <li><a href="https://ui.adsabs.harvard.edu/" target="_blank">NASA ADS Library</a></li>
+            <h3>Books And Papers</h3>
+            <a href="https://arxiv.org/" target="_blank">arXiv Scientific Publications</a><br />
+            <a href="https://ui.adsabs.harvard.edu/" target="_blank">NASA ADS Library</a>
 
-                        <h3>Citizen Science and Data</h3>
-                        <li><a href="https://www.zooniverse.org/" target="_blank">Zooniverse - Asteroid Data Hunter</a></li>
-                        <li><a href="https://data.nasa.gov/" target="_blank">NASA Open Data Portal</a></li>
-                        <li><a href="https://www.jpl.nasa.gov/" target="_blank">JPL Archives</a></li>
-
-
-                        <h3>Virtual And Augmented Reality</h3>
-                        <li><a href="https://psyche.asu.edu/get-involved/tools/" target="_blank">Psyche Virtual Mission Toolkit</a></li>
-                        <li><a href="https://solarsystem.nasa.gov/" target="_blank">NASA's Solar System Exploration</a></li>
-
-                        <h3>Educational Events and Outreach</h3>
-                        <li><a href="https://www.planetary.org/events" target="_blank">Planetary Society Events</a></li>
+            <h3>Citizen Science and Data</h3>
+            <a href="https://www.zooniverse.org/" target="_blank">Zooniverse - Asteroid Data Hunter</a><br />
+            <a href="https://data.nasa.gov/" target="_blank">NASA Open Data Portal</a><br />
+            <a href="https://www.jpl.nasa.gov/" target="_blank">JPL Archives</a>
 
 
-                        <h3>Collaborative Organizations</h3>
-                        <li><a href="https://www.maxar.com/" target="_blank">Maxar Technologies</a></li>
-                        <li><a href="http://ssl.mit.edu/" target="_blank">MIT's Space Systems Lab</a></li>
-                        <li><a href="https://www.psi.edu/" target="_blank">Planetary Science Institute</a></li>
+            <h3>Virtual And Augmented Reality</h3>
+            <a href="https://psyche.asu.edu/get-involved/tools/" target="_blank">Psyche Virtual Mission Toolkit</a><br />
+            <a href="https://solarsystem.nasa.gov/" target="_blank">NASA's Solar System Exploration</a>
 
-                        <ul>
+            <h3>Educational Events and Outreach</h3>
+            <a href="https://www.planetary.org/events" target="_blank">Planetary Society Events</a>
 
-                            <li><a href="https://psyche.asu.edu/" target="_blank">Official Psyche Mission Website</a></li>
-                            <li><a href="https://www.nasa.gov/psyche" target="_blank">NASA's Psyche Page</a></li>
-                            <li><a href="https://www.nasa.gov/multimedia/nasatv/" target="_blank">NASA TV</a></li>
+            <h3>Collaborative Organizations</h3>
+            <a href="https://www.maxar.com/" target="_blank">Maxar Technologies</a><br />
+            <a href="http://ssl.mit.edu/" target="_blank">MIT's Space Systems Lab</a><br />
+            <a href="https://www.psi.edu/" target="_blank">Planetary Science Institute</a><br />
+            <a href="https://psyche.asu.edu/" target="_blank">Official Psyche Mission Website</a><br />
+            <a href="https://www.nasa.gov/psyche" target="_blank">NASA's Psyche Page</a><br />
+            <a href="https://www.nasa.gov/multimedia/nasatv/" target="_blank">NASA TV</a><br />
 
-                            {/*<!-- Articles and News -->*/}
-                            <li><a href="https://blogs.nasa.gov/" target="_blank">NASA Blog Updates</a></li>
-                            <li><a href="https://www.space.com/" target="_blank">Space.com - Psyche Coverage</a></li>
-                            <li><a href="https://www.scientificamerican.com/" target="_blank">Scientific American</a></li>
+            {/*<!-- Articles and News -->*/}
+            <a href="https://blogs.nasa.gov/" target="_blank">NASA Blog Updates</a><br />
+            <a href="https://www.space.com/" target="_blank">Space.com - Psyche Coverage</a><br />
+            <a href="https://www.scientificamerican.com/" target="_blank">Scientific American</a><br />
 
-                            {/*<!-- Educational Resources -->*/}
-                            <li><a href="https://psyche.asu.edu/get-involved/psyche-inspired/" target="_blank">Psyche Inspired Program</a></li>
-                            <li><a href="https://www.nasa.gov/stem" target="_blank">NASA STEM Resources</a></li>
+            {/*<!-- Educational Resources -->*/}
+            <a href="https://psyche.asu.edu/get-involved/psyche-inspired/" target="_blank">Psyche Inspired Program</a><br />
+            <a href="https://www.nasa.gov/stem" target="_blank">NASA STEM Resources</a><br />
 
-                            {/*<!-- Videos and Media -->*/}
-                            <li><a href="https://www.youtube.com/user/NASAtelevision" target="_blank">NASA YouTube Channel</a></li>
+            {/*<!-- Videos and Media -->*/}
+            <a href="https://www.youtube.com/user/NASAtelevision" target="_blank">NASA YouTube Channel</a><br />
 
-                            {/*<!-- Interactive Tools -->*/}
-                            <li><a href="https://eyes.nasa.gov/apps/asteroids/" target="_blank">NASA Eyes on Asteroids</a></li>
+            {/*<!-- Interactive Tools -->*/}
+            <a href="https://eyes.nasa.gov/apps/asteroids/" target="_blank">NASA Eyes on Asteroids</a><br />
 
-                            {/*<!-- Podcasts and Audio Resources -->*/}
-                            <li><a href="https://www.nasa.gov/nasapodcasts" target="_blank">NASA?s On a Mission Podcast</a></li>
-                            <li><a href="https://www.planetary.org/planetary-radio" target="_blank">Planetary Radio by The Planetary Society</a></li>
+            {/*<!-- Podcasts and Audio Resources -->*/}
+            <a href="https://www.nasa.gov/nasapodcasts" target="_blank">NASA?s On a Mission Podcast</a><br />
+            <a href="https://www.planetary.org/planetary-radio" target="_blank">Planetary Radio by The Planetary Society</a><br />
 
-                            {/*<!-- Books and Papers -->*/}
-                            <li><a href="https://arxiv.org/" target="_blank">arXiv Scientific Publications</a></li>
-                            <li><a href="https://ui.adsabs.harvard.edu/" target="_blank">NASA ADS Library</a></li>
+            {/*<!-- Books and Papers -->*/}
+            <a href="https://arxiv.org/" target="_blank">arXiv Scientific Publications</a><br />
+            <a href="https://ui.adsabs.harvard.edu/" target="_blank">NASA ADS Library</a><br />
 
-                            {/*<!-- Citizen Science and Data -->*/}
-                            <li><a href="https://www.zooniverse.org/" target="_blank">Zooniverse - Asteroid Data Hunter</a></li>
-                            <li><a href="https://data.nasa.gov/" target="_blank">NASA Open Data Portal</a></li>
-                            <li><a href="https://www.jpl.nasa.gov/" target="_blank">JPL Archives</a></li>
+            {/*<!-- Citizen Science and Data -->*/}
+            <a href="https://www.zooniverse.org/" target="_blank">Zooniverse - Asteroid Data Hunter</a><br />
+            <a href="https://data.nasa.gov/" target="_blank">NASA Open Data Portal</a><br />
+            <a href="https://www.jpl.nasa.gov/" target="_blank">JPL Archives</a><br />
 
-                            {/*<!-- Virtual and Augmented Reality -->*/}
-                            <li><a href="https://psyche.asu.edu/get-involved/tools/" target="_blank">Psyche Virtual Mission Toolkit</a></li>
-                            <li><a href="https://solarsystem.nasa.gov/" target="_blank">NASA's Solar System Exploration</a></li>
+            {/*<!-- Virtual and Augmented Reality -->*/}
+            <a href="https://psyche.asu.edu/get-involved/tools/" target="_blank">Psyche Virtual Mission Toolkit</a><br />
+            <a href="https://solarsystem.nasa.gov/" target="_blank">NASA's Solar System Exploration</a><br />
 
-                            {/*<!-- Educational Events and Outreach -->*/}
-                            <li><a href="https://www.planetary.org/events" target="_blank">Planetary Society Events</a></li>
+            {/*<!-- Educational Events and Outreach -->*/}
+            <a href="https://www.planetary.org/events" target="_blank">Planetary Society Events</a><br />
 
-                            {/*<!-- Collaborative Organizations -->*/}
-                            <li><a href="https://www.maxar.com/" target="_blank">Maxar Technologies</a></li>
-                            <li><a href="http://ssl.mit.edu/" target="_blank">MIT's Space Systems Lab</a></li>
-                            <li><a href="https://www.psi.edu/" target="_blank">Planetary Science Institute</a></li>
+            {/*<!-- Collaborative Organizations -->*/}
+            <a href="https://www.maxar.com/" target="_blank">Maxar Technologies</a><br />
+            <a href="http://ssl.mit.edu/" target="_blank">MIT's Space Systems Lab</a><br />
+            <a href="https://www.psi.edu/" target="_blank">Planetary Science Institute</a><br />
 
-                            {/*<!-- Social Media and Forums -->*/}
-                            <li><a href="https://www.reddit.com/r/space/" target="_blank">Reddit - r/space</a></li>
-                            <li><a href="https://twitter.com/NASASolarSystem" target="_blank">NASA Solar System on Twitter/X</a></li>
-                            <li><a href="https://twitter.com/PsycheMission" target="_blank">Psyche Mission on Twitter/X</a></li>
-                        </ul>
+            {/*<!-- Social Media and Forums -->*/}
+            <a href="https://www.reddit.com/r/space/" target="_blank">Reddit - r/space</a><br />
+            <a href="https://twitter.com/NASASolarSystem" target="_blank">NASA Solar System on Twitter/X</a><br />
+            <a href="https://twitter.com/PsycheMission" target="_blank">Psyche Mission on Twitter/X</a><br /><br />
 
-
-                    </ul>
-                </nav>
-
-                <main>
-
-                    <div id="test">
-
-                    </div>
-                    <button size="large" type="button" onClick={handleButtonClick}>
-                        Return to Home page
-                    </button>
-                    {/*<h2><a href="#">Back To Simulation</a></h2> {/* <!-- This is supposed to lead back to the simulation but I don't have the filename for it yet-->*/}
-                </main>
-
-
-
-                {/*<footer>
-                <h2> Footer goes here</h2>
-                {/*<!-- to do: check what index.html footer looks like and copy that. -->*/}
-
-                {/*<ul class="social">
-                    {/*<!-- facebook -->*/}
-                {/*<li>
-                        <a target="_blank" href="https://www.facebook.com/MissionToPsyche">
-                            <img src="https://psyche.asu.edu/wp-content/themes/psyche/static/img/facebook.svg"/>
-                        </a>
-                    </li>
-                    {/*<!-- twitter -->*/}
-                {/*<li>
-                        <a target="_blank" href="https://x.com/MissionToPsyche">
-                            <img src="https://psyche.asu.edu/wp-content/themes/psyche/static/img/twitter.svg"/>
-                        </a>
-                    </li>
-
-                    {/*<!-- instagram -->*/}
-                {/*<li>
-                        <a target="_blank" href="https://www.instagram.com/missiontopsyche/">
-                            <img src="https://psyche.asu.edu/wp-content/themes/psyche/static/img/instagram.svg"/>
-                        </a>
-                    </li>
-                    {/*<!-- Youtube -->*/}
-                {/*<li>
-                        <a target="_blank" href="https://www.youtube.com/channel/UC2BGcbPW8mxryXnjQcBqk6A">
-                            {/*<img src="https://psyche.asu.edu/wp-content/themes/psyche/static/img/youtube.svg"/>*/}
-                {/*</a>
-                    </li>
-                </ul>
-
-
-                {/* <!-- If not: add this stuff in: -->
-                <!-- logo -->
-
-                <!-- social media tags -->*/}
-
-                {/*</footer>*/}
-            </>
-        </div>
+            <button size="large" type="button" onClick={handleButtonClick}>
+                Return to Home page
+            </button>
+        </>
 
     );
 }
