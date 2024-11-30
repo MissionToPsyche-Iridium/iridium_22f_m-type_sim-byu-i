ddb74d0c8c3b27fac7781c753b5abdadd4c8c1e8
Jerry Lane <55466939+JRLane-CS@users.noreply.github.com>
2024-11-30 16:44:37 -0600

    imported shared provider to app

--- MASS/mass.client/src/App.jsx
+++ MASS/mass.client/src/App.jsx
@@ -1,29 +1,32 @@
-import { useEffect, useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import MoreInfo from './components/MoreInfo';
 import MainMenu from './components/MainMenu';
 import Simulation from './components/Simulation';
+import ConstantParameter from './components/ConstantParameter';
+import VariableParameter from './components/VariableParameters';
+import { SharedProvider } from './components/SharedContext';
 import './App.css';
 
 function App() {
 
     return (
-     
-        <Router>
-            <Routes>
-                {/*<Route path="/" element={<Simulation />} />*/}
-                <Route path="/" element={<Home />} />
-                <Route path="/about-page" element={<About />} />
-                <Route path="/more-info-page" element={<MoreInfo />} />
-                <Route path="/main-menu-page" element={<Simulation />} />
-            </Routes>
-        </Router>
-        
-
+        <SharedProvider>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about-page" element={<About />} />
+                    <Route path="/more-info-page" element={<MoreInfo />} />
+                    <Route path="/main-menu-page" element={<MainMenu />} />
+                    <Route path="/simulation" element={<Simulation />} />
+                    <Route path="/constant-parameters" element={<ConstantParameter />} />
+                    <Route path="/variable-parameters" element={<VariableParameter />} />
+                </Routes>
+            </Router>
+        </SharedProvider >
     );
- 
 }
 
 export default App;
