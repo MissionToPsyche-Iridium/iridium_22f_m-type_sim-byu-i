6920313f704d98ee3ff0ae9e670c170c9b75957f
Jerry Lane <55466939+JRLane-CS@users.noreply.github.com>
2024-12-02 12:38:39 -0600

    added NASA logo to home screen

--- MASS/mass.client/src/components/Logo.jsx
+++ MASS/mass.client/src/components/Logo.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import Psyche_Mission_Logo from '../assets/Psyche_Mission_Logo.png'
+import Psyche_Mission_Logo from '../assets/Psyche_Mission_Logo.png';
+import NASA_Logo from '../assets/NASA_Logo.png';
 
 const styles = theme => ({
     img: {}
@@ -15,6 +16,7 @@ function Logo() {
             justifyContent: "center",
             height: "100%"
         }} >
+            <img src={NASA_Logo} alt="NASA Meatball Logo" />
             <img src={Psyche_Mission_Logo} alt="Psyche Mission Logo" />
         </div>
         
