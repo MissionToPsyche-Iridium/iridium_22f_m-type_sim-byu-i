4362f95d2cc9d44e7e63ff59055b04b8aeafca4f
Jerry Lane <55466939+JRLane-CS@users.noreply.github.com>
2024-12-10 18:57:39 -0600

    Adjusted Parameter Panel to handle 1080p

--- MASS/mass.client/src/components/ParameterComponent.jsx
+++ MASS/mass.client/src/components/ParameterComponent.jsx
@@ -4,9 +4,9 @@ const ParameterComponent = ({ title, subtitle, value }) => {
 
     return (
 
-        <div style={{ border: "1px solid #555", borderRadius: "3px", padding: "5px" }}>
-            <h5 style={{ padding: "0px", marginTop: "5px", marginBottom: "5px" }}>{title}</h5>
-            <p style={{ fontSize: "0.6rem", color: "#444" }}>{subtitle}</p>
+        <div style={{ border: "1px solid #555", borderRadius: "3px", padding: "2px" }}>
+            <h6 style={{ padding: "0px", marginTop: "2px", marginBottom: "2px" }}>{title}</h6>
+            <p style={{ fontSize: "0.5rem", color: "#444" }}>{subtitle}</p>
             <div
                 style={{
                     border: "2px solid #000",
